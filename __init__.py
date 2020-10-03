12c73a4553b27c34332df802df863c7821d1328b
J. Nick Koston <nick@koston.org>
2020-10-03 08:41:33 -0500

    lock

--- __init__.py
+++ __init__.py
@@ -28,28 +28,11 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Profiler from a config entry."""
 
-    async def _async_generate_profile(call: ServiceCall):
-        start_time = int(time.time() * 1000000)
-        hass.components.persistent_notification.async_create(
-            "The profile started. This notification will be updated when it is complete.",
-            title="Profile Started",
-            notification_id=f"profiler_{start_time}",
-        )
-        profiler = cProfile.Profile()
-        profiler.enable()
-        await asyncio.sleep(float(call.data.get(CONF_SECONDS)))
-        profiler.disable()
+    lock = asyncio.Lock()
 
-        cprofile_path = hass.config.path(f"profile.{start_time}.cprof")
-        callgrind_path = hass.config.path(f"callgrind.out.{start_time}")
-        await hass.async_add_executor_job(
-            _write_profile, profiler, cprofile_path, callgrind_path
-        )
-        hass.components.persistent_notification.async_create(
-            f"Wrote cProfile data to {cprofile_path} and callgrind data to {callgrind_path}",
-            title="Profile Complete",
-            notification_id=f"profiler_{start_time}",
-        )
+    async def _async_generate_profile(call: ServiceCall):
+        async with lock:
+            await _async_generate_profile(hass, call)
 
     async_register_admin_service(
         hass,
@@ -68,6 +51,30 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
     hass.services.async_remove(domain=DOMAIN, service=SERVICE_START)
     return True
+
+
+async def _async_generate_profile(hass: HomeAssistant, call: ServiceCall):
+    start_time = int(time.time() * 1000000)
+    hass.components.persistent_notification.async_create(
+        "The profile started. This notification will be updated when it is complete.",
+        title="Profile Started",
+        notification_id=f"profiler_{start_time}",
+    )
+    profiler = cProfile.Profile()
+    profiler.enable()
+    await asyncio.sleep(float(call.data.get(CONF_SECONDS)))
+    profiler.disable()
+
+    cprofile_path = hass.config.path(f"profile.{start_time}.cprof")
+    callgrind_path = hass.config.path(f"callgrind.out.{start_time}")
+    await hass.async_add_executor_job(
+        _write_profile, profiler, cprofile_path, callgrind_path
+    )
+    hass.components.persistent_notification.async_create(
+        f"Wrote cProfile data to {cprofile_path} and callgrind data to {callgrind_path}",
+        title="Profile Complete",
+        notification_id=f"profiler_{start_time}",
+    )
 
 
 def _write_profile(profiler, cprofile_path, callgrind_path):
